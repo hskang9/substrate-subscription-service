1fdd129bbd95246df1333881c64359bd71b4754a
hskang9 <hskang9@gmail.com>
2019-11-16 02:43:42 +0900

    roughly make subscriber

--- runtime/src/template.rs
+++ runtime/src/template.rs
@@ -1,30 +1,53 @@
-/// A runtime module template with necessary imports
+#![recursion_limit="128"]
+#![cfg_attr(not(feature = "std"), no_std)]
 
-/// Feel free to remove or edit this file as needed.
-/// If you change the name of this file, make sure to update its references in runtime/src/lib.rs
-/// If you remove this file, you can remove those references
-
-
-/// For more guidance on Substrate modules, see the example module
-/// https://github.com/paritytech/substrate/blob/master/srml/example/src/lib.rs
-
-use support::{decl_module, decl_storage, decl_event, dispatch::Result};
+use support::{decl_module, decl_storage, decl_event, dispatch::Result, ensure, Parameter};
 use system::ensure_signed;
+use rstd::prelude::*;
+use codec::{Encode, Decode};
 
-/// The module's configuration trait.
-pub trait Trait: system::Trait {
-	// TODO: Add other types and constants required configure this module.
-
-	/// The overarching event type.
+pub trait Trait: system::Trait + balances::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 
+#[derive(Encode, Decode, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct SubscriptionInfo<Balance: Parameter, AccountId: Parameter, Hash: Parameter, BlockNumber: Parameter> {
+	price: Balance,
+	to: AccountId,
+	blog_id: Hash,
+	period: BlockNumber,
+}
+
+impl<Balance: Parameter, AccountId: Parameter, Hash: Parameter, BlockNumber: Parameter> SubscriptionInfo<Balance, AccountId, Hash, BlockNumber> {
+	pub fn new(
+		price: Balance,
+		to: AccountId,
+		blog_id: Hash,
+		period: BlockNumber
+	) -> Self {
+		SubscriptionInfo { price, to, blog_id, period}
+	}
+
+	pub fn set_price(mut self, price: Balance) {
+		self.price = price;
+	}
+
+	pub fn set_recipient(mut self, recipient: AccountId) {
+		self.to = recipient;
+	}
+
+	pub fn set_blog_id(mut self, blog_id: Hash) {
+		self.blog_id = blog_id;
+	}
+}
+
 // This module's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
-		Something get(something): Option<u32>;
-		Calls get(call): map T::BlockNumber => Vec<T::AccountId> 
+		TotalSubscriptions get(total_subscriptions): u64;
+		Calls get(call): map T::BlockNumber => Vec<(T::AccountId, T::Hash)>;
+		SubscriptionOf: map (T::AccountId, T::Hash) => Option<SubscriptionInfo<T::Balance, T::AccountId, T::Hash, T::BlockNumber>>; 
 	}
 }
 
@@ -39,25 +62,27 @@ decl_module! {
 		fn on_initialize(block_number: T::BlockNumber) {
 			let subscribers = Self::call(block_number);
 			for i in subscribers {
-				if let Err(e) = Self::process_subscription(i) {
+				if let Err(e) = Self::process_subscription(i.0, i.1) {
 					sr_primitives::print(e);
 				}
 			}
 		}
 
-		// Just a dummy entry point.
-		// function that can be called by the external world as an extrinsics call
-		// takes a parameter of the type `AccountId`, stores it and emits an event
-		pub fn do_something(origin, something: u32) -> Result {
-			// TODO: You only need this if you want to check it was signed.
-			let who = ensure_signed(origin)?;
+		pub fn subscribe(origin, recipient: T::AccountId, period: T::BlockNumber, blog_id: T::Hash) -> Result {
+			let sender = ensure_signed(origin)?;
+			// pay initial price
 
-			// TODO: Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
-
-			// here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			// set subscription
+			let price = Self::to_balance(1, "one");
+			let new_subscription = SubscriptionInfo::new(price, recipient, blog_id, period);
+			<SubscriptionOf<T>>::insert((sender.clone(), blog_id), new_subscription);
+			let now = <system::Module<T>>::block_number();
+			if <Calls<T>>::exists(now+period) {
+				<Calls<T>>::mutate(now+period, |c: &mut Vec<(T::AccountId, T::Hash)>| c.push((sender.clone(), blog_id)));
+			} else {
+				<Calls<T>>::insert(now+period, vec![(sender, blog_id)]);
+			}
+			
 			Ok(())
 		}
 	}
@@ -72,9 +97,10 @@ decl_event!(
 );
 
 
-impl<T: trait> Module<T> {
+impl<T: Trait> Module<T> {
 
-	pub fn process_subscription(account: T::AccountId) -> Result {
+	pub fn process_subscription(account: T::AccountId, blog_id: T::Hash) -> Result {
+		
 		return Ok(());
 	}
 	
@@ -106,68 +132,3 @@ impl<T: trait> Module<T> {
 	}
 }
 
-/// tests for this module
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	use primitives::H256;
-	use support::{impl_outer_origin, assert_ok, parameter_types};
-	use sr_primitives::{
-		traits::{BlakeTwo256, IdentityLookup}, testing::Header, weights::Weight, Perbill,
-	};
-
-	impl_outer_origin! {
-		pub enum Origin for Test {}
-	}
-
-	// For testing the module, we construct most of a mock runtime. This means
-	// first constructing a configuration type (`Test`) which `impl`s each of the
-	// configuration traits of modules we want to use.
-	#[derive(Clone, Eq, PartialEq)]
-	pub struct Test;
-	parameter_types! {
-		pub const BlockHashCount: u64 = 250;
-		pub const MaximumBlockWeight: Weight = 1024;
-		pub const MaximumBlockLength: u32 = 2 * 1024;
-		pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
-	}
-	impl system::Trait for Test {
-		type Origin = Origin;
-		type Call = ();
-		type Index = u64;
-		type BlockNumber = u64;
-		type Hash = H256;
-		type Hashing = BlakeTwo256;
-		type AccountId = u64;
-		type Lookup = IdentityLookup<Self::AccountId>;
-		type Header = Header;
-		type Event = ();
-		type BlockHashCount = BlockHashCount;
-		type MaximumBlockWeight = MaximumBlockWeight;
-		type MaximumBlockLength = MaximumBlockLength;
-		type AvailableBlockRatio = AvailableBlockRatio;
-		type Version = ();
-	}
-	impl Trait for Test {
-		type Event = ();
-	}
-	type TemplateModule = Module<Test>;
-
-	// This function basically just builds a genesis storage key/value store according to
-	// our desired mockup.
-	fn new_test_ext() -> runtime_io::TestExternalities {
-		system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
-	}
-
-	#[test]
-	fn it_works_for_default_value() {
-		new_test_ext().execute_with(|| {
-			// Just a dummy test for the dummy funtion `do_something`
-			// calling the `do_something` function with a value 42
-			assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-			// asserting that the stored value is equal to what we stored
-			assert_eq!(TemplateModule::something(), Some(42));
-		});
-	}
-}
