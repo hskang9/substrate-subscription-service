69400d0dbda2e2b885b44a387d3f53a43e81eef9
hskang9 <hskang9@gmail.com>
2019-12-16 17:24:31 -0800

    feat: split runtime into two modules

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -62,7 +62,8 @@ pub type Hash = primitives::H256;
 pub type DigestItem = generic::DigestItem<Hash>;
 
 /// Used for the module template in `./template.rs`
-mod template;
+mod subscriber;
+mod subscription;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -256,7 +257,8 @@ construct_runtime!(
 		TransactionPayment: transaction_payment::{Module, Storage},
 		Sudo: sudo,
 		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module<T>, Call, Storage, Event<T>},
+		Subscription: subscription::{Module<T>, Call, Storage, Event<T>},
+		Subscriber: subscriber::{Module<T>, Call, Storage, Event<T>},
 		RandomnessCollectiveFlip: randomness_collective_flip::{Module, Call, Storage},
 	}
 );
